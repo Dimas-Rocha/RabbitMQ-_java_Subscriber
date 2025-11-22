96cc34dc147640f1594b170f4f86615fd1c46013
Dimas Rocha <dimas.dasp@gmail.com>
2025-11-21 23:38:40 -0300

    subscriber Service

---
+++ src/main/java/com/rmq/example/subscriber/Service/SubscriberService.java
@@ -0,0 +1,15 @@
+package com.rmq.example.subscriber.Service;
+
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriberService {
+    @RabbitListener(queues = "${rabbitmq.queuename}")
+    public void receiveMessage( Messagemessage) {
+        System.out.println(message);
+    }
+
+}
