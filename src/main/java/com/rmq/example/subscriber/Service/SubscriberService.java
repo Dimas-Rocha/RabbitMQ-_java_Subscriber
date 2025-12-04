f919108de133522750469cb3f9a29620d13eebc4
Dimas Rocha <dimas.dasp@gmail.com>
2025-12-03 21:58:27 -0300

    model and service review

--- src/main/java/com/rmq/example/subscriber/Service/SubscriberService.java
+++ src/main/java/com/rmq/example/subscriber/Service/SubscriberService.java
@@ -1,15 +1,41 @@
 package com.rmq.example.subscriber.Service;
 
 
+import com.fasterxml.jackson.core.io.JsonEOFException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rmq.example.subscriber.model.QueueMessage;
+import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class SubscriberService {
+
+    ObjectMapper objectMapper;
+
     @RabbitListener(containerFactory = "ListerContainerFactory", queues = "${rabbitmq.queuename}")
     public void receiveMessage( Messagemessage) {
-        System.out.println(new String( message.getBody()));
+        String jsonMessage = rmqMessageToString(message);
+        QueueMessage msgObject = new jsonToObject(jsonMessage, QueueMessage.class);
+        System.out.println(msgObject.toString());
     }
 
+    private String rmqMessageToString(Message message) {
+        return new String(message.getBody()),StandardCharsets.UTF_8.name();
+    }
+    private Object JsonObject(String JsonString, Class<?> clazz){
+        try{
+            return objectMapper.readValue(JsonString, clazz);
+        }catch (JsonEOFException e){
+            throw new RuntimeException(e);
+        }
+        @PostConstruct
+        public void init() {
+            ObjectMapper = new ObjectMapper();
+        }
+    }
 }
