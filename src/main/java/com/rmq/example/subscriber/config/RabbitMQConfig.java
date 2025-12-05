3d6bcc0ca01e2c107069e5937b55a2bebdfba639
Dimas Rocha <dimas.dasp@gmail.com>
2025-12-04 23:26:32 -0300

    Config Rabbit Queue

--- src/main/java/com/rmq/example/subscriber/config/RabbitMQConfig.java
+++ src/main/java/com/rmq/example/subscriber/config/RabbitMQConfig.java
@@ -1,12 +1,20 @@
 package com.rmq.example.subscriber.config;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
 import org.springframework.beans.CachedIntrospectionResults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 @Configuration
 public class RabbitMQConfig {
@@ -37,11 +45,55 @@ public class RabbitMQConfig {
     }
     @Bean
     public SimpleRabbitListenerContainerFactory listenerContainerFactory(
-            ConnectionFactory connectionFactoryI){
+            ConnectionFactory connectionFactory, RetryOperationsInterceptor retryOperationsInterceptor) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactoryI);
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConnectionFactory(retryOperationsInterceptor);
         return factory;
     }
-    )
+    @Bean
+    public AmqpAdmin amqpAdmin()throws Exception{
+        return new RabbitAdmin(connectionFactory());
+    }
+    @Bean
+    public Queue createQueue() throws Exception{
+        Queue q = QueueBuilder.durable().build(queueName)
+                .build();
+        amqpAdmin().declareQueue(q);
+
+        return q;
+    }
+
+
+    @Bean
+    Queue boqQueue() throws Exception{
+        Queue q = QueueBuilder.durable(name:"boq."+ queueName)
+          .build();
+        amqpAdmin().declareQueue(q);
+        return q;
+    }
+
+    @Bean
+    public RepublishMessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate){
+        RepublishMessageRecoverer republishMessageRecoverer = new RepublishMessageRecoverer(rabbitTemplate);
+        recoverer.setErrorRoutingKeyPrefix("boq.");
+        return recoverer;
+    }
+
+    @Bean
+    public RetryOperationsInterceptor retryOperationsInterceptor(
+        RepublishMessageRecoverer recoverer){
+        RetryOperationsInterceptor interceptor = RetryInterceptorBuilder
+                .stateless()
+                .maxAttempts(2)
+                .backOffOptions(initialInterval: 2000, multipiler: 1, maxinterval: 100000)
+                .recoverer(recoverer)
+                .build();
+        return interceptor;
+
+    }
+
+
+
 
 }
